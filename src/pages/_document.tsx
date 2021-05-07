84ef086b52eaadff70ff69bd801c1e34e10c5e45
Connor M <63883267+Connor406@users.noreply.github.com>
2021-05-07 15:14:47 -0400

    adds Google Analytics

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,5 +1,6 @@
 import NextDocument, { Html, Head, Main, NextScript } from "next/document";
 import { ColorModeScript } from "@chakra-ui/react";
+require("dotenv").config();
 
 export default class Document extends NextDocument {
   render() {
@@ -19,6 +20,21 @@ export default class Document extends NextDocument {
             crossOrigin="anonymous"
           />
           <link href="index.css" rel="stylesheet" />
+          {/* <!-- Global site tag (gtag.js) - Google Analytics --> */}
+          <script
+            async
+            src="https://www.googletagmanager.com/gtag/js?id=G-8PRJVJ30R8"
+          />
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+
+            gtag('config', ${process.env.GOOGLE_ANALYTICS});`,
+            }}
+          />
         </Head>
         <body>
           {/* Make Color mode to persists when you refresh the page. */}
